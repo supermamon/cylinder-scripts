dd70018c5f1280149c3dcf35e4193f5a402a9c8a
Raymond Velasquez <monvelasquez@gmail.com>
2014-02-18 10:54:30 +0800

    updated "Ant Lines (Vertical)"
    v1.2
    - fix inside folders

--- supermamon/Ant Lines (Vertical).lua
+++ supermamon/Ant Lines (Vertical).lua
@@ -1,15 +1,16 @@
 --[[ ******************************************************************
-Ant Lines (Vertical) v1.1
+Ant Lines (Vertical) v1.2
 by @supermamon (github.com/supermamon/cylinder-scripts/)
 
 Alternating Slide up/down transition
 
+v1.2 2014-02-18: Fixed for folders
 v1.1 2014-02-16: Compatibility update for Cylinder v0.13.2.15
-v1.0 2014-02-15 First Release
+v1.0 2014-02-15: First Release
 		
 ******************************************************************* ]]
-local fade = dofile("include/fade.lua")
-local stayPut = dofile("include/stayPut.lua")
+local fade		= dofile("include/fade.lua")
+local stayPut	= dofile("include/stayPut.lua")
 
 -- MAIN --
 return function(page, offset, screen_width, screen_height)
@@ -23,14 +24,22 @@ return function(page, offset, screen_width, screen_height)
 
 	-- ** ICON EFFECTS ** --
     local i = 0
-	local direction = -1 
+	local direction = 1 
+	local lastX = page.width
     while true do
         i = i + 1
         local icon = page[i]
         if icon == nil then break end
 		
 		-- reverse the direction alternately 
-		if (i % 2 == 0) then direction = 1 else direction = -1 end
+		direction = -direction 
+		
+		-- reset the direction if in a new row (fix for folders)
+		if (lastX > icon.x) then 
+			direction = -1
+		end
+		lastX = icon.x
+		
 		icon:translate(0, direction*percent*page.height, 0)
 		
     end	
