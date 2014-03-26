0d1ac60178dce1209f5b880e1c410fe1f92a70f0
Raymond Velasquez <monvelasquez@gmail.com>
2014-03-26 17:21:14 +0800

    More optimizations
    More optimizations

--- supermamon/Page Twist.lua
+++ supermamon/Page Twist.lua
@@ -4,6 +4,7 @@ by @supermamon (github.com/supermamon/cylinder-scripts/)
 
 Twisting effect between the current and next page
 
+v1.2 2014-03-26: Cleanup
 v1.1 2014-02-16: Compatibility update for Cylinder v0.13.2.15
 v1.0 2014-02-15: First release.
 ******************************************************************* ]]
@@ -11,7 +12,6 @@ v1.0 2014-02-15: First release.
 local fade = dofile("include/fade.lua")
 
 return function(page, offset, screen_width, screen_height)
-		
 	-- track progress
 	local percent = offset/page.width 
 	
@@ -21,12 +21,6 @@ return function(page, offset, screen_width, screen_height)
 	
 	-- ** PAGE EFFECTS ** --
 	fade(page,percent)
-	
-	--local x = math.abs(percent)
-    --page:translate(x, 0, 0)
     page:rotate(-2/3*angle, 1, 0, 0)
-    --page:translate(-x, 0, 0)	
-	
-	
-		
+
 end
